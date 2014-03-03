e08d53b48d85726559e77de6ba69f8ec018bac79
jmigh <jmigithub@gmail.com>
2014-03-03 17:52:35 +0100

    coveralls

--- commerce/test/test_helper.rb
+++ commerce/test/test_helper.rb
@@ -1,6 +1,8 @@
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'coveralls'
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -13,3 +15,4 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
