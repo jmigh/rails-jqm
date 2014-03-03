ae93ebf6c77e880d782d2bf5370a0530ec33bc59
jmigh <jmigithub@gmail.com>
2014-03-03 17:17:32 +0100

    commerce [ci skip]

---
+++ commerce/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Commerce::Application.config.secret_key_base = '540f8835db797003e3cafd8b826f152f9be82d8fca3d978d954c234e08f3e20fc41d0fa05ace0084b71c57f234785bfb96385a18f7dd0dac2ce2848e599387a9'
